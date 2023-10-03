71d5bf5a0c3da8e65cce41339318b031dddec6c1
razor <mrrazish@gmail.com>
2023-10-03 19:00:14 +1000

    Merge pull request #1151 from SomaZ/rend2
    Rend2

--- codemp/rd-vanilla/G2_misc.cpp
+++ codemp/rd-vanilla/G2_misc.cpp
@@ -26,10 +26,9 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 #include "server/server.h"
 #include "ghoul2/g2_local.h"
 
+#include "tr_local.h"
 #ifdef _G2_GORE
 #include "ghoul2/G2_gore.h"
-
-#include "tr_local.h"
 
 #define GORE_TAG_UPPER (256)
 #define GORE_TAG_MASK (~255)
@@ -557,6 +556,7 @@ void G2_TransformModel(CGhoul2Info_v &ghoul2, const int frameNum, vec3_t scale, 
 	vec3_t			correctScale;
 	qboolean		firstModelOnly = qfalse;
 
+#ifdef _G2_GORE
 	if ( cg_g2MarksAllModels == NULL )
 	{
 		cg_g2MarksAllModels = ri->Cvar_Get( "cg_g2MarksAllModels", "0", 0, "" );
@@ -567,7 +567,7 @@ void G2_TransformModel(CGhoul2Info_v &ghoul2, const int frameNum, vec3_t scale, 
 	{
 		firstModelOnly = qtrue;
 	}
-
+#endif
 
 	VectorCopy(scale, correctScale);
 	// check for scales of 0 - that's the default I believe
@@ -1506,6 +1506,7 @@ void G2_TraceModels(CGhoul2Info_v &ghoul2, vec3_t rayStart, vec3_t rayEnd, Colli
 	shader_t		*cust_shader;
 	qboolean		firstModelOnly = qfalse;
 
+#ifdef _G2_GORE
 	if ( cg_g2MarksAllModels == NULL )
 	{
 		cg_g2MarksAllModels = ri->Cvar_Get( "cg_g2MarksAllModels", "0", 0, "" );
@@ -1516,6 +1517,7 @@ void G2_TraceModels(CGhoul2Info_v &ghoul2, vec3_t rayStart, vec3_t rayEnd, Colli
 	{
 		firstModelOnly = qtrue;
 	}
+#endif
 
 	// walk each possible model for this entity and try tracing against it
 	for (i=0; i<ghoul2.size(); i++)
@@ -1561,6 +1563,7 @@ void G2_TraceModels(CGhoul2Info_v &ghoul2, vec3_t rayStart, vec3_t rayEnd, Colli
 		}
 
 		lod = G2_DecideTraceLod(ghoul2[i],useLod);
+#ifdef _G2_GORE
 		if ( skipIfLODNotMatch )
 		{//we only want to hit this SPECIFIC LOD...
 			if ( lod != useLod )
@@ -1568,7 +1571,7 @@ void G2_TraceModels(CGhoul2Info_v &ghoul2, vec3_t rayStart, vec3_t rayEnd, Colli
 				continue;
 			}
 		}
-
+#endif
 		//reset the quick surface override lookup
 		G2_FindOverrideSurface(-1, ghoul2[i].mSlist);
 
