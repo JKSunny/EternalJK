1e6ebf7a0e84fcdfbe74f7456b2d239cf4c98c9c
Futuza <darthfutuza@hotmail.com>
2026-03-10 00:11:21 -0600

    adding WP_FireGenericArc() and associated functionality.  New arcangle allows a weapon to be fired in an arc defined by an arc angle, and does hit trace checking to make sure those in the arc aren't behind cover etc.

--- codemp/game/g_local.h
+++ codemp/game/g_local.h
@@ -1555,6 +1555,7 @@ int			 WP_GetWeaponMOD( gentity_t *ent, int firemode );
 int			 WP_GetWeaponSplashMOD( gentity_t *ent, int firemode );
 float		 WP_GetWeaponRange( gentity_t *ent, int firemode );
 float		 WP_GetWeaponDecayRate(gentity_t* ent, int firemode);
+float		WP_GetWeaponArcAngle(gentity_t* ent, int firemode);
 float		 WP_GetWeaponSpeed( gentity_t *ent, int firemode );
 float		 WP_GetWeaponArmorPenetration(gentity_t* ent, int firemode);
 double		 WP_GetWeaponSplashRange( gentity_t *ent, int firemode );
