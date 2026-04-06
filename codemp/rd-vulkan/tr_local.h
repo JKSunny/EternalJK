ef9eee3224505cfeb7647f5aa286050a9076b67b
JKSunny <60109631+JKSunny@users.noreply.github.com>
2026-04-06 11:22:10 +0200

    Vulkan: Upstream changes from quake3e https://github.com/ec-/Quake3e/commit/63d6d7402d78254bebe28035006fe600f645c8de

--- codemp/rd-vulkan/tr_local.h
+++ codemp/rd-vulkan/tr_local.h
@@ -55,6 +55,7 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 	#define USE_VBO_GHOUL2
 	#define USE_VBO_MDV	
 	#define USE_VBO_SS
+	#define USE_VBO_GRID		/* put SF_GRID to VBO */
 #endif
 
 
@@ -2613,7 +2614,9 @@ void		R_CreateDefaultShadingCmds( image_t *image );
 //
 // tr_surface.c
 //
+#ifdef USE_VBO_GRID
 void		RB_SurfaceGridEstimate(srfGridMesh_t *cv, int *numVertexes, int *numIndexes);
+#endif
 
 /*
 ====================================================================
