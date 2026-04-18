4d76ae55651ce98dca4cc693074dbef5f333c24a
JKSunny <60109631+JKSunny@users.noreply.github.com>
2026-04-18 19:48:07 +0200

    Vulkan: Mark RT_BEAM and RT_ENT_CHAIN surface types unsupported for entity merging. Inspired by https://github.com/SomaZ/OpenJK/commit/b2d9ad7bf6b1a00ccac8dc521c3a55d9e2eeab5a
    Also merge: https://github.com/SomaZ/OpenJK/commit/41553ebf988ad097594c488cb77b2afbc85a7e8b

--- codemp/rd-vulkan/tr_surface.cpp
+++ codemp/rd-vulkan/tr_surface.cpp
@@ -2170,6 +2170,7 @@ void RB_SurfaceEntity( const surfaceType_t *surfType ) {
 		RB_SurfaceOrientedQuad();
 		break;
 	case RT_BEAM:
+		allow_merge = qfalse;
 		RB_SurfaceBeam();
 		break;
 	case RT_ELECTRICITY:
@@ -2209,6 +2210,8 @@ void RB_SurfaceEntity( const surfaceType_t *surfType ) {
 
 				RB_SurfaceEntity(surfType);
 			}
+
+			allow_merge = qfalse;
 		}
 		break;
 	default:
